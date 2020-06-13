5382af43f9e2ab3c1814aa91229a1fb1edcd7f68
Piyush Maheswari <piyush.maheswari@testbook.com>
2020-06-13 22:34:05 +0530

    Added Bottom Nav and Nav Components

--- app/src/main/java/com/androiddevs/mvvmnewsapp/NewsActivity.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/NewsActivity.kt
@@ -2,11 +2,17 @@ package com.androiddevs.mvvmnewsapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.main.activity_news.*
 
 class NewsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_news)
+
+
+        bottomNavigationView.setupWithNavController(NavHostFragment.findNavController())
     }
 }
