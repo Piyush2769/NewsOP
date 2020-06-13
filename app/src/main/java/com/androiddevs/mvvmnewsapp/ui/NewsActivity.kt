df837065d91bc1c57ae775bd863e6bbcbe8d304a
Piyush Maheswari <piyush.maheswari@testbook.com>
2020-06-13 22:39:36 +0530

    Code Refactoring

--- app/src/main/java/com/androiddevs/mvvmnewsapp/ui/NewsActivity.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/ui/NewsActivity.kt
@@ -1,9 +1,10 @@
-package com.androiddevs.mvvmnewsapp
+package com.androiddevs.mvvmnewsapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
+import com.androiddevs.mvvmnewsapp.R
 import kotlinx.android.synthetic.main.activity_news.*
 
 class NewsActivity : AppCompatActivity() {
